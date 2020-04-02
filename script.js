db8cf28675fd7aaaf773fec34a541d35d9ddccdd
Varun V <varunvirupaksha321@gmail.com>
2020-04-02 18:59:47 +0530

    added lyrics.ovh api url

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ const form = document.getElementById('form');
 const search = document.getElementById('search');
 const result = document.getElementById('result');
 const more = document.getElementById('more');
+
+const apiURL = 'https://api.lyrics.ovh';
+
+// Search by song or artist
+async function searchSongs(term) {
+  const res = await fetch(`${apiURL}/suggest/${term}`);
+  const data = await res.json();
+
+  showData(data);
+}
